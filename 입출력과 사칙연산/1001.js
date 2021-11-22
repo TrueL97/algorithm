f885476ab76cb27f6c38c0174acc471ae7fdb383
leeyunsik <leeys9932@naver.com>
2021-11-22 23:25:38 +0900

    a

--- 입출력과 사칙연산/1001.js
+++
@@ -1,6 +0,0 @@
-const fs = require("fs");
-const input = fs.readFileSync("/dev/stdin").toString().trim().split(" "); //한줄 입력
-
-let a = Number(input[0]);
-let b = Number(input[1]);
-console.log(a - b);
--- 입출력과 사칙연산/10998.js
+++
@@ -1,6 +0,0 @@
-const fs = require("fs");
-const input = fs.readFileSync("/dev/stdin").toString().split(" "); //한줄 입력
-
-let a = input[0];
-let b = input[1];
-console.log(a * b);
