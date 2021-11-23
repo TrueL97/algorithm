81d0c30a51bba56e39a3a7b84b41ce0b5f26480a
leeyunsik <leeys9932@naver.com>
2021-11-23 21:19:56 +0900

    알고리즘

--- 프로그래머스/신규 아이디 추천.playground/Contents.swift
+++ 프로그래머스/신규 아이디 추천.playground/Contents.swift
@@ -1,6 +1,55 @@
 import UIKit
 
-
 func solution(_ new_id:String) -> String {
-    return ""
+    var myID: String = new_id
+
+    //1차 소문자로 치환
+    myID = myID.lowercased()
+
+    //2차 소문자 숫자 - _ . 을 제외한 문자 제거
+    var newID: String = ""
+    for i in myID {
+        if i.isLetter || i.isNumber || i == "-" || i == "_" || i == "." {
+            newID.append(i)
+        }
+    }
+
+    //3차 .이 두번이상 연속된 부분을 하나의 마침표로 치환
+    while newID.contains("..") {
+        newID = newID.replacingOccurrences(of: "..", with: ".")
+    }
+
+    //4차 .이 처음이나 끝에 위치할때 제거
+    while newID.hasPrefix(".") {
+        newID.removeFirst()
+    }
+
+    while newID.hasSuffix(".") {
+        newID.removeLast()
+    }
+
+    //5차 빈 문자열일때 a를 대입
+    if newID == "" {
+        newID = "a"
+    }
+
+    //6차 길이 16이상이면 15다음문제 제거 만약 제거후 .이 끝에 위치하면 마침표 제거
+    if newID.count >= 16 {
+        let index = newID.index(newID.startIndex, offsetBy: 15)
+        newID = String(newID[newID.startIndex..<index])
+        if newID.hasSuffix(".") {
+            newID.removeLast()
+        }
+    }
+
+    //7차 길이가 2이하면 길이가 3이될때까지 끝문자 반복
+    if newID.count <= 2 {
+        while newID.count != 3 {
+            newID = newID + String(newID.last!)
+        }
+    }
+
+    return newID
 }
+var a = solution("...!@BaT#*.....y.abcdefghijklm")
+print(a)
