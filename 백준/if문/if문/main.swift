e6d5e963d5bd0391269f61da1b1de2ffaed577ae
leeyunsik <leeys9932@naver.com>
2021-12-18 14:55:07 +0900

    1330, 9498, 2753, 14681, 2884

---
+++ 백준/if문/if문/main.swift
@@ -0,0 +1,86 @@
+
+import Foundation
+
+//1330
+//let input = readLine()!
+//let intputArr = input.split(separator: " ")
+//let a = Int(intputArr[0])!
+//let b = Int(intputArr[1])!
+//
+//if a > b {
+//    print(">")
+//}else if a < b{
+//    print("<")
+//}else{
+//    print("==")
+//}
+
+
+////9498
+//let input = Int(readLine()!)! //언래핑 사용하는이유, nil이나 숫자가 아닌게 들어있을 수 있기 때문, 반드시 int여야한다
+//if input >= 90 {
+//    print("A")
+//}else if input < 90 && input >= 80 {
+//    print("B")
+//} else if input < 80 && input >= 70 {
+//    print("C")
+//} else if input < 70 && input >= 60 {
+//    print("D")
+//} else {
+//    print("F")
+//}
+
+//2753
+//let input = Int(readLine()!)!
+//
+//if input % 4 == 0 {
+//    if input % 100 == 0 && input % 400 != 0 {
+//    print("0")
+//    } else {
+//        print("1")
+//    }
+//
+//} else{
+//    print("0")
+//}
+
+//14681
+//let input1 = readLine()!
+//let input2 = readLine()!
+//let a = Int(input1)!
+//let b = Int(input2)!
+//
+//if a > 0 && b > 0 {
+//    print("1")
+//}
+//if a < 0 && b > 0 {
+//    print("2")
+//}
+//if a < 0 && b < 0 {
+//    print("3")
+//}
+//if a > 0 && b < 0 {
+//    print("4")
+//}
+
+//2884
+//let input = readLine()!
+//let intputArr = input.split(separator: " ")
+//let a = Int(intputArr[0])!
+//let b = Int(intputArr[1])!
+//
+//var hour = a - 1
+//var min = 60 + b - 45
+//if hour == -1 {
+//    hour = 23
+//}
+//if min - 60 >= 0 {
+//    min = min - 60
+//    hour = hour + 1
+//}
+//if hour == 24 {
+//    hour = 0
+//}
+//print(String(hour) + " " + String(min))
+
+
---
+++ 프로그래머스/크레인 인형뽑기 게임.playground/Contents.swift
@@ -0,0 +1,5 @@
+import UIKit
+
+func solution(_ board:[[Int]], _ moves:[Int]) -> Int {
+    return 0
+}
