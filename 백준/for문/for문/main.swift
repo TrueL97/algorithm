ba415b0f16a153dfa4f91533adc431ab85b5ab7f
leeyunsik <leeys9932@naver.com>
2021-12-19 14:03:58 +0900

    2739, 10950, 8393, 2741, 2742, 11021, 11022, 2438, 2439, 10871

--- 백준/for문/for문/main.swift
+++ 백준/for문/for문/main.swift
@@ -7,5 +7,102 @@
 
 import Foundation
 
-print("Hello, World! ")
+//2739
+//let input = Int(readLine()!)!
+//
+//for i in 1..<10 {
+//    print("\(input) * \(i) = \(input * i)")
+//}
+
+//10950
+//let input = Int(readLine()!)!
+//
+//for _ in 1...input {
+//    var arr:[Int] = []
+//    arr = (readLine()?.split(separator: " ").map{Int($0)!})!
+//    print(arr[0] + arr[1])
+//}
+
+//8393
+//let input = Int(readLine()!)!
+//var sum = 0
+//for i in 1...input{
+//    sum = sum + i
+//}
+//print(sum)
+
+//15552
+
+//2741
+//let input = Int(readLine()!)!
+//
+//for i in 1...input {
+//    print(i)
+//}
+
+//2742
+//let input = Int(readLine()!)!
+//
+//for i in (1...input).reversed() {
+//    print(i)
+//}
+
+//11021
+//let input = Int(readLine()!)!
+//
+//for i in 1...input {
+//    var arr:[Int] = []
+//    arr = (readLine()?.split(separator: " ").map{Int($0)!})!
+//    print("Case #\(i): \(arr[0] + arr[1])")
+//}
+
+//11022
+//let input = Int(readLine()!)!
+//
+//for i in 1...input {
+//    var arr:[Int] = []
+//    arr = (readLine()?.split(separator: " ").map{Int($0)!})!
+//    print("Case #\(i): \(arr[0]) + \(arr[1]) = \(arr[0] + arr[1])")
+//}
+
+//2438
+//let input = Int(readLine()!)!
+//
+//for i in 1...input{
+//    var star = ""
+//    for j in 1...i{
+//        star = star + "*"
+//    }
+//    print(star)
+//}
+
+//2349
+//let input = Int(readLine()!)!
+//
+//for i in 1...input{
+//    var star = ""
+//    for j in 1...input+1 - i{
+//        star = star + " "
+//
+//    }
+//    for k in 1...i{
+//    star = star + "*"
+//}
+//    print(star)
+//}
+
+//10871
+//let input = readLine()!
+////print(input)
+//let arr = input.split(separator: " ")
+//let num = Int(arr[0])!
+//let X = Int(arr[1])!
+//
+//print(readLine()!
+//        .split(separator: " ")
+//        .map{Int(String($0))!}
+//        .filter{$0 < X}
+//        .map{String($0)}
+//        .joined(separator: " ")
+//)
 
