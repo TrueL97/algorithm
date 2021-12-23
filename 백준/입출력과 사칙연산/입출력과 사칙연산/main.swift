3ef578b5f10546fd45592335b5c8a12ff83406d9
YunL97 <leeys9932@naver.com>
2021-12-23 18:34:01 +0900

    2577, 1008, 10430, 2588

---
+++ 백준/입출력과 사칙연산/입출력과 사칙연산/main.swift
@@ -0,0 +1,90 @@
+//
+//  main.swift
+//  입출력과 사칙연산
+//
+//  Created by 이윤식 on 2021/12/23.
+//
+
+import Foundation
+
+////2577
+//print("Hello World!")
+//
+//
+////10171
+//let cat = #"""
+//\    /\
+// )  ( ')
+//(  /  )
+// \(__)|
+//"""#
+//
+//print(cat)
+//
+//
+////1008
+//let input = readLine()!
+//let arr = input.split(separator: " ")
+//let A = Int(arr[0])!
+//let B = Int(arr[1])!
+//let C = Int(arr[2])!
+//
+//let one = (A+B)%C
+//let two = ((A%C)+(B%C))%C
+//
+//let three = (A*B)%C
+//let four = ((A%C)*(B%C))%C
+//
+//print(one)
+//print(two)
+//print(three)
+//print(four)
+//
+////10430
+//let input = readLine()!
+//let arr = input.split(separator: " ")
+//let A = Int(arr[0])!
+//let B = Int(arr[1])!
+//let C = Int(arr[2])!
+//
+//let one = (A+B)%C
+//let two = ((A%C)+(B%C))%C
+//
+//let three = (A*B)%C
+//let four = ((A%C)*(B%C))%C
+//
+//print(one)
+//print(two)
+//print(three)
+//print(four)
+//
+////2588
+//let input = readLine()!
+//let arr = input.split(separator: " ")
+//let A = Int(arr[0])!
+//let B = Int(arr[1])!
+//let C = Int(arr[2])!
+//
+//let one = (A+B)%C
+//let two = ((A%C)+(B%C))%C
+//
+//let three = (A*B)%C
+//let four = ((A%C)*(B%C))%C
+//
+//print(one)
+//print(two)
+//print(three)
+//print(four)
+
+
+//2588
+//let input1 = Int(readLine()!)!
+//let input2 = Int(readLine()!)!
+//
+//let three = input1 * (input2 % 10)
+//let four = input1 * (input2 % 100 - input2 % 10) / 10
+//let five = input1 * (input2 / 100)
+//print(three)
+//print(four)
+//print(five)
+//print(input1 * input2)
