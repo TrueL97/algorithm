df58658296af7c6dec452eba8ee2aa879e036562
YunL97 <leeys9932@naver.com>
2021-12-24 16:33:55 +0900

    4673

--- 백준/함수/함수/main.swift
+++ 백준/함수/함수/main.swift
@@ -9,4 +9,45 @@ import Foundation
 
 //4673
 
+var arr:[Int] = []
 
+
+for  i in 1...10000 {
+    arr.append(d((i)))
+
+}
+for j in 1...10000 {
+    if !arr.contains(j) {
+        print(j)
+    }
+}
+
+func d(_ n:Int) -> Int {
+    var sum = n
+    var number = n
+    while n != 0 {
+        sum = sum + number%10
+        number = number / 10
+
+        if number == 0 {
+            break
+        }
+    }
+//    print(sum)
+    return sum
+}
+
+
+//var s: Set<Int> = []
+//for i in 1...10000
+//{ s.insert(d(i)) }
+//for j in 1...10000
+//{ if !s.contains(j) { print(j) } }
+//func d(_ n:Int) -> Int
+//{ var sum = n, now = n
+//    while now != 0 {
+//        sum += now%10
+//        now /= 10
+//
+//    }
+//    return sum }
