531069537cc1920382f826cc361359bb36713e53
YunL97 <leeys9932@naver.com>
2021-12-26 20:01:19 +0900

    문자열

--- 백준/문자열/문자열/main.swift
+++ 백준/문자열/문자열/main.swift
@@ -10,5 +10,44 @@
 
 import Foundation
 
-print("Hello, World!")
+//11654
+//print(Character(readLine()!).asciiValue!)
+
+//11720
+//let input = readLine()!
+//let input1 = readLine()!
+//var sum = 0
+//for i in input1{
+//    sum = sum + Int(String(i))!
+//}
+//
+//print(sum)
+
+//10809
+//let s = Array(readLine()!)
+//for i in Character("a").asciiValue!...Character("z").asciiValue! {
+//     var char = Character(UnicodeScalar(i))
+//    if s.contains(char){
+//        print("\(s.index(of: char)!)",terminator: " ")
+//    }else{
+//    print("-1", terminator: " ")
+//    }
+//}
+
+//2675
+//let input = Int(readLine()!)!
+//for i in 1...input {
+//    var input1 = readLine()!.split(separator: " ")
+//    var repeatt = Int(String(input1[0]))!
+//    var str = Array(input1[1])
+//    for i in 0...str.count-1  {
+//        for j in 0...repeatt-1{
+//            print(str[i], terminator: "")
+//        }
+//
+//    }
+//    print("")
+//}
+
+
 
