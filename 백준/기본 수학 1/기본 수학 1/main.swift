852d126699cfc8cdcaeaab6b03a149a496c6a6ba
YunL97 <leeys9932@naver.com>
2021-12-29 21:27:59 +0900

    공부

--- 백준/기본 수학 1/기본 수학 1/main.swift
+++ 백준/기본 수학 1/기본 수학 1/main.swift
@@ -8,5 +8,63 @@
 
 import Foundation
 
-print("Hello, World!")
+//1712
+//let input = readLine()!.split(separator: " ").map{Int($0)!}
+//
+//
+////여기 오류가 난이유: input[2] - input[1] 이 0이 될수도 있기 때문
+////var result = (input[0]/(input[2] - input[1])) + 1
+//
+//if input[1]  >= input[2] {
+//    print(-1)
+//}else {
+//    print((input[0] / (input[2] - input[1])) + 1)
+//}
+
+
+//2292
+//let input = Int(readLine()!)!
+//
+//let a = input
+//var sum = 1
+//var i = 0
+//while true {
+//    sum = sum + 6 * i
+//    if a <= sum {
+//        print(i + 1)
+//        break
+//    }
+//
+//
+////    print(sum)
+//
+//    i = i + 1
+//}
+
+//1193
+//let input = Int(readLine()!)!
+//
+//var ja = 1
+//var mo = 1
+//var pl = 1
+//if input > 1{
+//    for i in 2...input {
+//        mo = mo + pl
+//        ja = ja - pl
+//        if 1 > ja {
+//            ja = 1
+//            pl *= -1
+//            continue
+//        }
+//        if 1 > mo {
+//            mo = 1
+//            pl *= -1
+//            continue
+//        }
+//
+//    }
+//}
+//print("\(ja)/\(mo)")
+
+
 
