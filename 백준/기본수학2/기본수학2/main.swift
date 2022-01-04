bf6a769818916751324d8332f6b3349a0a057e17
YunL97 <leeys9932@naver.com>
2022-01-04 09:40:10 +0900

    1978

--- 백준/기본수학2/기본수학2/main.swift
+++ 백준/기본수학2/기본수학2/main.swift
@@ -7,5 +7,69 @@
 
 import Foundation
 
-print("Hello, World!")
+//1978
+
+//var sosu:Set<Int> = []
+//
+//for i in 1...1000{
+//    sosu.insert(i)
+//}
+//
+//var number = 2
+//var count = 2
+//while true {
+//    var one = number * count
+//    sosu.remove(one)
+//
+//    count = count + 1
+//
+//    if one > 1000 {
+//        number = number + 1
+//        count = 2
+//    }
+//    if number > 500 {
+//        break
+//    }
+//}
+//sosu.remove(1)
+////print(sosu)
+//
+//let input = Int(readLine()!)!
+//let arr = readLine()!.split(separator: " ").map{ Int($0)!}
+//var result = 0
+//
+//    for i in 0...arr.count - 1{
+//    if sosu.contains(arr[i]){
+//        result = result + 1
+//    }
+//}
+//
+//print(result)
+//*****************************************************************
+//let input = Int(readLine()!)!
+//let arr = readLine()!.split(separator: " ").map{ Int($0)!}
+//var result = 0
+//
+//    for i in arr {
+//        var tr = false
+//        if i == 1 {
+//            continue
+//        }
+//        if i == 2 {
+//            result = result + 1
+//            continue
+//        }
+//        for j in 2...i-1 {
+//            if i % j == 0 {
+//                tr = true
+//            }
+//        }
+//        if tr == false {
+//            result = result + 1
+//        }
+//
+//
+//}
+//
+//print(result)
 
