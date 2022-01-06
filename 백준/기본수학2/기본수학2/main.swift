a4fc4a900d140ab513382aef2c12765dadcd3037
YunL97 <leeys9932@naver.com>
2022-01-06 11:21:08 +0900

    2939, 4948, 9020

--- 백준/기본수학2/기본수학2/main.swift
+++ 백준/기본수학2/기본수학2/main.swift
@@ -32,7 +32,7 @@ import Foundation
 //    }
 //}
 //sosu.remove(1)
-////print(sosu)
+//print(sosu)
 //
 //let input = Int(readLine()!)!
 //let arr = readLine()!.split(separator: " ").map{ Int($0)!}
@@ -113,26 +113,26 @@ import Foundation
 //}
 
 //11653
-var result = Int(readLine()!)!
-
-var i = 2
-while true {
-    if result == 1 {
-        break
-    }
-
-    if result % i != 0 {
-        i = i + 1
-    }else {
-        //i로 나눠질때
-        print(i)
-        result = result / i
-        if result == 1 {
-            break
-        }
-    }
-
-}
+//var result = Int(readLine()!)!
+//
+//var i = 2
+//while true {
+//    if result == 1 {
+//        break
+//    }
+//
+//    if result % i != 0 {
+//        i = i + 1
+//    }else {
+//        //i로 나눠질때
+//        print(i)
+//        result = result / i
+//        if result == 1 {
+//            break
+//        }
+//    }
+//
+//}
 //---------------------------
 //var N = Int(String(readLine()!))!
 //
@@ -167,4 +167,129 @@ while true {
 //
 //}
 
+//1929
+//var sosu:Set<Int> = []
+//
+//for i in 2...1000000{
+//    sosu.insert(i)
+//}
+//
+//var number = 2
+//var count = 2
+//while true {
+//    var one = number * count
+//    sosu.remove(one)
+//
+//    count = count + 1
+//
+//    if one > 1000000 {
+//        number = number + 1
+//        count = 2
+//    }
+//    if number > 500000 {
+//        break
+//    }
+//}
+//
+//let input = readLine()!.split(separator: " ").map{ Int($0)!}
+//
+//var arr = [Int]()
+//for i in input[0]...input[1] {
+//    if sosu.contains(i){
+//        arr.append(i)
+//    }
+//}
+//
+//for i in arr {
+//    print(String(i))
+//}
 
+//4948
+//var sosu:Set<Int> = []
+//
+//for i in 2...246912{
+//    sosu.insert(i)
+//}
+//
+//var number = 2
+//var count = 2
+//while true {
+//    let one = number * count
+//    sosu.remove(one)
+//
+//    count = count + 1
+//
+//    if one > 246912 {
+//        number = number + 1
+//        count = 2
+//    }
+//    if number > 123456 {
+//        break
+//    }
+//}
+//
+//while true{
+//
+//let input = Int(readLine()!)!
+//    if input == 0 {
+//        break
+//    }
+//var arr = [Int]()
+//for i in input + 1...input * 2 {
+//    if sosu.contains(i){
+//        arr.append(i)
+//    }
+//}
+//
+//    print(arr.count)
+//
+//}
+
+//9020
+var sosu:Set<Int> = []
+for i in 2...10000{
+sosu.insert(i)
+}
+var number = 2
+var count = 2
+while true {
+let one = number * count
+sosu.remove(one)
+count = count + 1
+
+    if one > 10000 {
+number = number + 1
+count = 2
+}
+if number > 5000 {
+break
+}
+}
+
+
+let input = Int(readLine()!)!
+for _ in 1...input{
+    let input2 = Int(readLine()!)!
+    var arr = [Int]()
+    var arr2 = [String]()
+    
+    for i in 2...input2 {
+        if sosu.contains(i) {
+            arr.append(i)
+        }
+    }
+    
+    if arr.contains(input2 / 2){
+        print("\(input2 / 2) \(input2 / 2)")
+    }else {
+        for j in 0...arr.count-1 {
+            for k in j...arr.count - 1 {
+                if arr[j] + arr[k] == input2 {
+                    arr2.append("\(arr[j]) \(arr[k])")
+                }
+            }
+        }
+        print(arr2.last!)
+    }
+
+}
