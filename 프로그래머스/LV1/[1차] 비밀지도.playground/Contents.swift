63f2c00c3c8b676bbaff9f14798b22c0377ba748
YunL97 <leeys9932@naver.com>
2022-01-21 13:55:19 +0900

    for문 사용 array insert 사용

--- 프로그래머스/LV1/[1차] 비밀지도.playground/Contents.swift
+++ 프로그래머스/LV1/[1차] 비밀지도.playground/Contents.swift
@@ -6,7 +6,7 @@ func solution(_ n:Int, _ arr1:[Int], _ arr2:[Int]) -> [String] {
     var answer: [String] = []
     
     var answer1:[[Int]] = Array(repeating: Array(repeating: 0, count: 0), count: n)
-    var answer2: [[Int]] = []
+    var answer2:[[Int]] = Array(repeating: Array(repeating: 0, count: 0), count: n)
     
     var arr1 = arr1
     var arr2 = arr2
@@ -15,9 +15,7 @@ func solution(_ n:Int, _ arr1:[Int], _ arr2:[Int]) -> [String] {
     for i in 0...arr1.count - 1 {
         var k = arr1[i]
         var j = 0
-        
         while n != j{
-            print(j)
             if k == 0 {
                 answer1[i].insert(0, at: 0)
             }else {
@@ -25,15 +23,42 @@ func solution(_ n:Int, _ arr1:[Int], _ arr2:[Int]) -> [String] {
                 answer1[i].insert(zeroone, at: 0)
                 k = k / 2
             }
-            
-            
             j = j + 1
         }
-        print(answer1)
         
     }
 
+    for i in 0...arr2.count - 1 {
+        var k = arr2[i]
+        var j = 0
+        while n != j{
+            if k == 0 {
+                answer2[i].insert(0, at: 0)
+            }else {
+                var zeroone = k % 2
+                answer2[i].insert(zeroone, at: 0)
+                k = k / 2
+            }
+            j = j + 1
+        }
+    }
     
+//    print(answer1)
+//    print(answer2)
+//    
+    
+    for i in 0...n - 1 {
+        var hash = ""
+        for j in 0...n - 1 {
+            if answer1[i][j] == 0 && answer2[i][j] == 0 {
+                hash = hash + " "
+            }else {
+                hash =  hash + "#"
+            }
+        }
+        answer.append(hash)
+    }
+    print(answer)
     
     return answer
 }
